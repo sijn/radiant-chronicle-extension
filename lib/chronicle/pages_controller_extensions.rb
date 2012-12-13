69c9dba8c09edaa1884ec81c0cc80230c3cda5fd
Thomas Wouters <thomas@sijn.nl>
2012-12-13 23:11:28 +0100

    fix for undefined method Admin::PagesController'

--- lib/chronicle/pages_controller_extensions.rb
+++ lib/chronicle/pages_controller_extensions.rb
@@ -2,7 +2,7 @@ module Chronicle::PagesControllerExtensions
   def self.included(base)
     base.class_eval {
       include InstanceMethods
-      alias_method_chain :announce_saved, :show_page
+#      alias_method_chain :announce_saved, :show_page
       helper 'admin/preview'
       include Admin::PreviewHelper
     }
